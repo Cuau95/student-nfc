7233c50bb0b2ea6d91776a8ab603d6c589c3338a
Cuau95 <elzua_ecm@live.com.mc>
2019-05-29 14:02:20 -0500

    add new endpoint

--- src/main/java/com/example/student/nfc/domain/service/StudentNFCRelationService.java
+++ src/main/java/com/example/student/nfc/domain/service/StudentNFCRelationService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StudentNFCRelationService {
-    
+
     private StudentService studentService;
     private StudentNFCRelationRepository repository;
     private ServiceHelper helper;
@@ -22,45 +22,55 @@ public class StudentNFCRelationService {
         this.repository = repository;
         this.helper = helper;
     }
-    
+
     public StudentNFCRelationEntity saveNFCId(StudentNFCRelationEntity nfc) {
         return repository.save(nfc);
     }
-    
-    public StudentNFCRelation saveStudentNFCRelation (String idStudent) {
+
+    public StudentNFCRelation saveStudentNFCRelation(String idStudent) {
         StudentNFCRelation relation = new StudentNFCRelation();
         Student student = getStudentById(idStudent);
         String idNFC = getNFCId().getIdNfc();
         relation.setAlumno(student);
         relation.setIdNfc(idNFC);
-        if(student != null && idNFC != null){
+        if (student != null && idNFC != null) {
             updateStudent(student.getId(), idNFC);
         }
         return relation;
     }
-    
+
     public Student getStudentById(String id) {
         return studentService.getStudentById(id);
     }
-    
+
     public String getIdStudentByidNFC(String idNFC) {
         return repository.findByIdNfc(idNFC).getIdAlumno();
     }
-    
+
     public List<StudentNFCRelation> getStudentNFCRelation() {
-        List<StudentNFCRelationEntity> entitiesList = (List<StudentNFCRelationEntity>)repository.findAll();
+        List<StudentNFCRelationEntity> entitiesList = (List<StudentNFCRelationEntity>) repository.findAll();
         return helper.transformFromEntityToModel(entitiesList);
     }
-    
+
+    public String deleteNFCRelation(String idNFC) {
+        try {
+            StudentNFCRelationEntity relation = repository.findByIdNfc(idNFC);
+            repository.delete(relation);
+            return relation.getIdNfc();
+        } catch(Exception ex){
+            return null;
+        }
+    }
+
     private void updateStudent(String idStudent, String idNFC) {
         StudentNFCRelationEntity relationEntity = new StudentNFCRelationEntity();
         relationEntity.setIdAlumno(idStudent);
         relationEntity.setIdNfc(idNFC);
         saveNFCId(relationEntity);
     }
-    
+
     private StudentNFCRelationEntity getNFCId() {
         return repository.findByIdAlumno(null).get(0);
     }
-    
+
 }
--- src/main/java/com/example/student/nfc/infrestructure/controller/StudentNFCRelationController.java
+++ src/main/java/com/example/student/nfc/infrestructure/controller/StudentNFCRelationController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.FOUND;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,9 +53,15 @@ public class StudentNFCRelationController {
         return new ResponseEntity<>(service.getIdStudentByidNFC(idNFC), OK);
     }
     
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/relation")
     public ResponseEntity<List<StudentNFCRelation>> getAllRelations() {
         return new ResponseEntity<>(service.getStudentNFCRelation(), OK);
+    }
+    
+    @DeleteMapping("/nfc/{idNFC}")
+    public ResponseEntity<String> deleteNFCRelation(@PathVariable String idNFC) {
+        return new ResponseEntity<>(service.deleteNFCRelation(idNFC), FOUND);
     }
     
 }
